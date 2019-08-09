cbe30302e279174d92a344b58d7765bde1588de9
Amy Fong <amyhfong@gmail.com>
2019-08-09 19:38:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
 def prime?(integer)
     prime_nums = [2, 3, 5, 7, 9, 11, 13]
     dividers = Array(14..100)
-    if integer > 1 && !integer.even?
+    if prime_nums.include?(integer)
+      true
+    elsif prime_nums.each {|num|
+      integer % num != 0
+      true
+    }
+
 end
