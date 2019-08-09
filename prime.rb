2ab2f3175b772f55dbb1ea24bd276641b75d0f39
Amy Fong <amyhfong@gmail.com>
2019-08-09 19:23:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(integer)
   prime_nums = [2, 3, 5, 7, 11]
   if integer > 1 && prime_nums.include?(integer)
+    true
   elsif integer > 1 && !integer.even? && prime_nums.each {|num|
     integer % num != 0
   }
